349018e6fb706e7626c4a9cd684810c172696518
Coder <60108915+NVcoder24@users.noreply.github.com>
2021-01-21 17:39:08 +0400

    Update email_works.py

--- email_works.py
+++ email_works.py
@@ -2,87 +2,91 @@ import smtplib
 import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import urllib.request
-import datetime
-
-sender = None
-receiver = None
-password = None
-logging = False
-
-current_time = datetime.datetime.now()
 
 
-def log(success=False):
-    if logging and success:
-        time = f"{current_time.hour}:{current_time.minute}:{current_time.second}"
-        date = f"{current_time.day}.{current_time.month}.{current_time.year}"
-        print(f"Message on {time} - {date} [From: {sender}, to: {receiver}]")
+class email_works:
+    data = {}
+    debug = True
 
+    def __init__(self):
+        self.data = {
+        "context": ssl.create_default_context(),
+        "sender": None,
+        "password": None,
+        "logged": False
+        }
 
-def send(header, text=None, html=None, url=None):
-    if sender is not None:
-        if receiver is not None:
-            if password is not None:
-                global textg
-                global htmlg
-                global urlg
+    def debug(self, data=False):
+        self.debug = data
 
-                textg = text
-                htmlg = html
-                urlg = url
+    def login(self, data):
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=self.data["context"]) as server:
+                server.login(data["sender"], data["password"])
+                self.data["logged"] = True
+                self.data["sender"] = data["sender"]
+                self.data["password"] = data["password"]
+        except Exception as e:
+            if self.debug:
+                print(f"error was handled in login(): {e}")
+            return e
 
-                message = MIMEMultipart("alternative")
+    def send(self, data):
+        if self.check_data(self.data) == True:
+            try:
+                with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=self.data["context"]) as server:
+                    server.login(self.data["sender"], self.data["password"])
+                    server.sendmail(self.data["sender"], data["receiver"], data["message"].as_string())
+            except Exception as e:
+                if self.debug:
+                    print(f"error was handled in send(): {e}")
+                return e
 
-                message["Subject"] = header
-                message["From"] = sender
-                message["To"] = receiver
-
-                if textg is not None:
-                    textg = """{text}""".format(text=textg)
-                    part1 = MIMEText(textg, "plain")
-                    message.attach(part1)
-
-                if html is not None:
-                    textg = ""
-                    htmlg = """{html}""".format(html=htmlg)
-                    part2 = MIMEText(htmlg, "html")
-                    message.attach(part2)
-
-                if url is not None:
-                    htmlg = ""
-                    textg = ""
-                    web = urllib.request.urlopen(urlg)
-                    data = web.read()
-                    data = str(data)
-                    data = data.split("\\n")
-                    i = ""
-                    for char in data:
-                        i = i + char
-                    data = i
-
-                    data = data.split("b'")
-                    i = ""
-                    for char in data:
-                        i = i + char
-                    data = i
-
-                    part3 = MIMEText(data, "html")
-                    message.attach(part3)
-
-                context = ssl.create_default_context()
-                try:
-                    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-                        server.login(sender, password)
-                        server.sendmail(sender, receiver, message.as_string())
-                        log(True)
-                except Exception as e:
-                    print(e)
-                    print("If you need to solve this problem go issues on github. Or text me on email))")
-                    print("https://github.com/NVcoder24/email_works")
+    def check_data(self, data):
+        if data["password"] != None:
+            if data["sender"] != None:
+                if data["logged"] != False:
+                    return True
+                else:
+                    return False
             else:
-                print("configuration error: password")
+                return False
         else:
-            print("configuration error: reciver email")
-    else:
-        print("configuration error: sender email")
+            return False
+
+    def send_text(self, header, text, receiver):
+        if self.check_data(self.data) == True:
+            message = MIMEMultipart("alternative")
+            message["Subject"] = header
+            message["From"] = self.data["sender"]
+            message["To"] = receiver
+            message.attach(MIMEText(text, "plain"))
+
+            info = {"receiver":receiver,
+                    "message":message}
+
+            self.send(info)
+        else:
+            if self.debug:
+                print("you need to login first!")
+
+    def send_html(self, header, html, receiver):
+        if self.check_data(self.data) == True:
+            message = MIMEMultipart("alternative")
+            message["Subject"] = header
+            message["From"] = self.data["sender"]
+            message["To"] = receiver
+            message.attach(MIMEText(html, "html"))
+
+            info = {"receiver": receiver,
+                    "message": message}
+
+            self.send(info)
+        else:
+            if self.debug:
+                print("you need to login first!")
+
+            self.send(info)
+        else:
+            if self.debug == True:
+                print("you need to login first!")
